7bba3e3d1b74afacbd6f35a84784b0bdf4da2bd0
lxXian <1255642377@qq.com>
2017-10-31 23:36:06 +0800

    修改界面

--- app/src/main/java/com/duhui/xian/kaola/MainActivity.java
+++ app/src/main/java/com/duhui/xian/kaola/MainActivity.java
@@ -1,13 +1,23 @@
 package com.duhui.xian.kaola;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fm=getSupportFragmentManager();
+        Fragment fragment=fm.findFragmentById(R.id.fragment_home_xml);
+        if(fragment==null){
+            fragment=new homeFragement();
+            fm.beginTransaction().add(R.id.child_content,fragment).commit();
+        }
     }
 }
+
+
---
+++ app/src/main/java/com/duhui/xian/kaola/courseFragement.java
@@ -0,0 +1,23 @@
+package com.duhui.xian.kaola;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by min on 2017/10/31.
+ */
+
+public class courseFragement extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
+        View v =inflater.inflate(R.layout.fragement_course,container,false);
+        return v;
+    }
+}
