a01066af95a0de577655d89483f9331fd4e8a693
xiaoxian-long <1066144006@qq.com>
2017-12-20 17:59:54 +0800

    公司版本2017-12-20

--- app/src/main/java/com/duhui/xian/kaola/bookshelf/BookshelfDatabaseHelper.java
+++ app/src/main/java/com/duhui/xian/kaola/bookshelf/BookshelfDatabaseHelper.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
  * Created by min on 2017/11/8.
  */
 
-public class bookshelfDatabaseHelper extends SQLiteOpenHelper {
+public class BookshelfDatabaseHelper extends SQLiteOpenHelper {
 
     //primary key 将id列设为主键    autoincrement表示id列是自增长的
     public static final String CREATE_BOOKlIST="create table BookList("+
@@ -20,7 +20,7 @@ public class bookshelfDatabaseHelper extends SQLiteOpenHelper {
     private Context mContext;
 
     //构造方法：第一个参数Context，第二个参数数据库名，第三个参数cursor允许我们在查询数据的时候返回一个自定义的光标位置，一般传入的都是null，第四个参数表示目前库的版本号（用于对库进行升级）
-    public  bookshelfDatabaseHelper(Context context,String name,SQLiteDatabase.CursorFactory factory , int version){
+    public BookshelfDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory , int version){
         super(context,name ,factory,version);
         mContext = context;
     }
